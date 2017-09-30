4d1c5fd57df3e0a395afaa7e0bbbb4df005487dd
martin-forsingdal <s154170@student.dtu.dk>
2017-09-30 16:47:28 +0200

    Add files via upload

---
+++ fconv.m
@@ -0,0 +1,12 @@
+function [y] = fconv(h,x)
+%FCONV Summary of this function goes here
+%   Detailed explanation goes here
+
+len=length([h,x])-1;
+h=[h,zeros(1,len-length(h))];
+x=[x,zeros(1,len-length(x))];
+H=fft(h);
+X=fft(x);
+y=real(ifft(H*X));
+end
+
