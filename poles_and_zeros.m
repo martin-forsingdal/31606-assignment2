1276c57ea70fcf3b923b2c3e7f23bd574f0dff61
martin-forsingdal <s154170@student.dtu.dk>
2017-10-05 13:52:42 +0200

    Tilføjer funktion til at repræsenstere poler og nulpunkter i z-domænet

---
+++ poles_and_zeros.m
@@ -0,0 +1,11 @@
+function [] = poles_and_zeros(poles,zeros)
+%POLES_AND_ZEROS plot poles/zeros and the spectrum
+%   poles=vector containing poles of the system
+%   zeros=vector containing zeros of the system
+
+figure('Position',[100 300 800 600],'Color','white');
+zplane(zeros,poles);
+figure('Position',[1000 300 800 600],'Color','white');
+freqz(poly(zeros),poly(poles));
+end
+
---
+++ uge5.m
@@ -0,0 +1,3 @@
+poles=[0;0];
+zeros=[1+1i;1-1i];
+poles_and_zeros(poles,zeros);
