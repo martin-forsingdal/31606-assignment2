0de409cb09fea7738c364753493fda18e51da092
martin-forsingdal <s154170@student.dtu.dk>
2017-10-05 14:19:51 +0200

    no message

--- uge5.m
+++ uge5.m
@@ -1,3 +1,9 @@
-poles=[0;0];
-zeros=[1+1i;1-1i];
+%% init
+clc
+clear
+close all
+
+%% running sum filter of order 3
+poles=[0,0];
+zeros=[1/2+(sqrt(3)/2)*i,1/2+-(sqrt(3)/2)*i];
 poles_and_zeros(poles,zeros);
