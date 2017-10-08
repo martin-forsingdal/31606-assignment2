341d329cf64ab1aa3ce3bbc0700863d04f1ede01
Jonas Ørnskov Nielsen <Jonas@Jonas-MacBook-Pro-2.local>
2017-10-08 11:31:09 +0200

    spectrum_maker

---
+++ spectrum_maker.m
@@ -0,0 +1,18 @@
+function [Y, freq] = spectrum_maker(signal, fs)
+% Function computes complex-valued Fourier-spectrum of signal "signal" with
+% sampling frequency fs.
+% Input: signal to transform, "signal", sampling frequency of signal, fs:
+% (signal, fs).
+% Output: Complex-valued spectrum, Y, frequency vector: (Y, freq):
+
+% Compute spectrum
+Y = fft(signal);
+
+% Perform scaling of spectrum
+Y = Y/length(Y);
+
+% Frequency vector
+F_0 = fs/length(Y);
+freq = 0:F_0:fs-F_0;
+
+end
