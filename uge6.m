7fa2435d642e64329c06563e46f2556505f281b4
martin-forsingdal <s154170@student.dtu.dk>
2017-10-14 11:52:43 +0200

    Opdatering til 1.2

--- uge6.m
+++ uge6.m
@@ -9,7 +9,7 @@ Gp = 2;
 Gs = 11;
 
 % specify the frequencies
-w_p = 8;    
+w_p = 10;    
 w_s = 15;
 
 % specify the highest frequency to process (Nyquist!)
@@ -26,4 +26,29 @@ w_s_norm = w_s/w_max;
 [B, A] = butter(N, Wn);
 
 %%
+fs=4*pi*35; %omega_n = 35
+figure(1);
 freqz(B, A);
+nuller=roots(B);
+poler=roots(A);
+figure(2);
+zplane(nuller,poler);
+
+%% 1.2
+%% generate signal
+[t, s] = generate_sinusoid(1,100,0,fs,0.1);
+
+s_filt1=filter(B,A,s);
+s_filt2=filter(B,A,fliplr(s_filt1));
+%% Plot sound signal
+% Create time vector
+fig1 = figure(1);
+plot(t,fliplr(s_filt2),'r');
+xlabel('Time [t]','FontSize',15);
+ylabel('Signal [a.u.]','FontSize',15);
+grid on;
+hold on;
+plot(t,s,'b');
+set(gcf,'Position',[100 100 800 500]);
+set(gca,'Fontsize',12)
+hold off;
