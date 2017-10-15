08aa06b3551131c5b6e92151e67b578a7207a117
Jonas Ørnskov Nielsen <Jonas@Jonas-MacBook-Pro-2.local>
2017-10-15 15:59:04 +0200

    Updated Exercise 1.2 from hands on 5. Updates: - comments - exports of figures - deleted exercise 1.1 which should not be handed

--- Exercise1.m
+++ Exercise1.m
@@ -1,84 +1,13 @@
-% Exercise 1.1
-clear all;
-clc
-close all;
-
-%% Filter of order 3
-zero3 = roots([1,1,1,1]);
-pole3 = roots([1,0,0,0]);
-
-%% Filter of order 5
-zero5 = roots([1,1,1,1,1,1]);
-pole5 = roots([1,0,0,0,0,0]);
-
-%% Plot zplane
-% First for order 3
-figure(1);
-zplane(zero3,pole3)
-figure(2);
-B3 = poly(zero3);
-A3 = poly(pole3);
-freqz(B3, A3);
-% Then for order 5
-figure(3);
-zplane(zero5,pole5);
-figure(4);
-B5 = poly(zero5);
-A5 = poly(pole5);
-freqz(B5, A5);
-
-%% Moving average order 3
-close all;
-zero3 = roots(1/(3+1)*[1,1,1,1]);
-pole3 = roots([1,0,0,0]);
-
-%% Moving average order 5
-zero5 = roots(1/(5+1)*[1,1,1,1,1,1]);
-pole5 = roots([1,0,0,0,0,0]);
-
-%% Plot zplane
-% First for order 3
-figure(1);
-zplane(zero3,pole3)
-figure(2);
-B3 = poly(zero3);
-A3 = poly(pole3);
-freqz(B3, A3);
-% Then for order 5
-figure(3);
-zplane(zero5,pole5);
-figure(4);
-B5 = poly(zero5);
-A5 = poly(pole5);
-freqz(B5, A5);
-% Interchange poles and zeros
-
-%% Place zeros
-zero = [exp(j*0.1*pi) exp(-j*0.1*pi) exp(j*0.1*pi) exp(-j*0.1*pi)]';
-B = poly(zero);
-A = 1;
-figure(1);
-zplane(zero);
-figure(2);
-freqz(B, A);
-
-%% Place poles with a distance R
-R = 0.99;
-pole = R*zero;
-A = poly(pole);
-figure(3);
-zplane(zero,pole);
-figure(4);
-freqz(B, A);
-
 %% Exercise 1.2
 % Clear variables and command window
-%clear all;
+clear all;
 clc;
 close all;
-%% sample frequency used for filtering of sinusoid
+
+%% Sampling frequency used for filtering of sinusoid
 fs = 1000;
-%%
+
+%% Define filters
 % For lowpass set zero = -1 and pole = 0;
 zero = -1;
 pole = 0;
@@ -117,12 +46,14 @@ freqz(B_allpass, A_allpass);
 ylim([-20 5]);
 
 %% Create sinusoid:
+close all;
 f = 20;
-%create a sinusoid with frequency of 20 Hz
+% Create a sinusoid with frequency of 20 Hz
 [t, sinusoid] = generate_sinusoid(1,f,0,fs,1);
+
 % Lowpass filter sinusoid
 sinusoid_low = real(filter(B_low,A_low,sinusoid));
-% Plot the alpass filtered sinusoid
+% Plot the low pass filtered sinusoid
 figure(1);
 plot(t,sinusoid,'r');
 hold on;
@@ -130,10 +61,12 @@ plot(t,sinusoid_low,'b');
 legend('Signal','Filtered signal');
 hold off;
 xlim([0 1/f*4]);
+grid on;
+%hgexport(gcf,'sin_low');
 
 % Highpass filter sinusoid
 sinusoid_high = real(filter(B_high,A_high,sinusoid));
-% Plot the alpass filtered sinusoid
+% Plot the high pass filtered sinusoid
 figure(2);
 plot(t,sinusoid,'r');
 hold on;
@@ -141,10 +74,12 @@ plot(t,sinusoid_high,'b');
 legend('Signal','Filtered signal');
 hold off;
 xlim([0 1/f*4]);
+grid on;
+%hgexport(gcf,'sin_high');
 
 % Allpass filter sinusoid
 sinusoid_allpass = real(filter(B_allpass,A_allpass,sinusoid));
-% Plot the alpass filtered sinusoid
+% Plot the all pass filtered sinusoid
 figure(3);
 plot(t,sinusoid,'r');
 hold on;
@@ -152,7 +87,11 @@ plot(t,sinusoid_allpass,'b');
 legend('Signal','Filtered signal');
 hold off;
 xlim([0 1/f*4]);
+grid on;
+%hgexport(gcf,'sin_all');
+
 %% Read sound file
+close all;
 filename = 'x.wav';
 [soundsignal, fs] = audioread(filename);
 dur = length(soundsignal)/fs;
@@ -161,10 +100,9 @@ t = 0:1/fs:dur-1/fs;
 sound(soundsignal,fs);
 pause(dur);
 
-
 %% Filter soundsignal
-%NOTE: the filter are the same, since they scale with the sampling
-%frequency!
+% NOTE: the filter are the same, since they scale with the sampling
+% frequency!
 % Lowpass filter soundsignal
 soundsignal_low = real(filter(B_low,A_low,soundsignal));
 % Plot the alpass filtered soundsignal
@@ -175,6 +113,8 @@ plot(t,soundsignal,'r');
 legend('Filtered signal','Signal');
 hold off;
 xlim([0 dur]);
+grid on;
+%hgexport(gcf,'sound_low');
 
 % Highpass filter soundsignal
 soundsignal_high = real(filter(B_high,A_high,soundsignal));
@@ -186,6 +126,8 @@ plot(t,soundsignal_high,'b');
 legend('Signal','Filtered signal');
 hold off;
 xlim([0 dur]);
+grid on;
+%hgexport(gcf,'sound_high');
 
 % Allpass filter soundsignal
 soundsignal_allpass = real(filter(B_allpass,A_allpass,soundsignal));
@@ -197,6 +139,8 @@ plot(t,soundsignal,'r');
 legend('Filtered signal','Signal');
 hold off;
 xlim([0 dur]);
+grid on;
+%hgexport(gcf,'sound_all');
 
 %% Play the lowpass filtered soundsignal
 sound(soundsignal_low,fs);
